b05df6c77d1f2ae21f60375b082cf2160a26d44f
Fadeelay <fadlullahlawal2@gmail.com>
2022-12-19 13:34:00 -0800

    reset to 98

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,13 +1,6 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-/**
- * File: holberton.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x04-pointers_arrays_strings directory.
- */
-
 #include <stdlib.h>
 
 int _putchar(char c);
