e8163844318f6612aeac68a18068336e3c966b5b
Fadeelay <fadlullahlawal2@gmail.com>
2023-01-02 14:45:39 -0800

    memcpy and memset

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  *_memcpy - a function that copies memory area
- *@dest: memory where is stored
- *@src: memory where is copied
+ *@dest: this is where memory is stored
+ *@src: this is memory where is copied
  *@n: number of bytes
  *
  *Return: copied memory with n byted changed
