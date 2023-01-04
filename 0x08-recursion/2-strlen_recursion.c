16ae88c716708c5e5fd359ba3aaaaef717c28303
Fadeelay <fadlullahlawal2@gmail.com>
2023-01-04 15:53:02 -0800

    recursion tasks completed

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - returns the length of a string.
+ * _strle8n_recursion - returns the length of a given string.
  * @s: string
  * Return: the length of a string.
  */
