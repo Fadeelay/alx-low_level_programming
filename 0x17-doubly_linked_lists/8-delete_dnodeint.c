52273ae5347c76ddc101b527084595847f5eea2d
Fadeelay <fadlullahlawal2@gmail.com>
2023-03-17 15:38:55 -0700

    Double linked lists

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,116 +1,42 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
-  * delete_dnodeint_at_index - ...
-  * @head: ...
-  * @index: ...
-  *
-  * Return: ...
-  */
+ * delete_dnodeint_at_index - Deletes a node from a dlistint_t
+ *                            at a given index.
+ * @head: A pointer to the head of the dlistint_t.
+ * @index: The index of the node to delete.
+ *
+ * Return: Upon success - 1.
+ *         Otherwise - -1.
+ */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *current = NULL, *temp = NULL;
-	unsigned int length = 0;
+	dlistint_t *tmp = *head;
 
-	if (head && *head)
+	if (*head == NULL)
+		return (-1);
+
+	for (; index != 0; index--)
 	{
-		length = dlistint_len(*head);
-		if (index > length)
+		if (tmp == NULL)
 			return (-1);
-
-		if (index == 0)
-			return (delete_first_dnode(head));
-
-		current = get_dnodeint_at_index(*head, index);
-		if (current)
-		{
-			temp = current;
-			if (length - 1 == index)
-				current->prev->next = current->next;
-			else
-			{
-				current->prev->next = current->next;
-				current->next->prev = current->prev;
-			}
-
-			free(temp);
-			return (1);
-		}
+		tmp = tmp->next;
 	}
 
-	return (-1);
-}
-
-/**
-  * delete_first_dnode - Remove the first node of a doubly linked list
-  * @head: The head of the doubly linked list
-  *
-  * Return: 1 if is deleted
-  */
-int delete_first_dnode(dlistint_t **head)
-{
-	dlistint_t *current = *head, *temp = NULL;
-
-	temp = current;
-	if (current->next)
+	if (tmp == *head)
 	{
-		current = current->next;
-		current->prev = temp->prev;
-		*head = current;
+		*head = tmp->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
 	}
+
 	else
 	{
-		*head = NULL;
+		tmp->prev->next = tmp->next;
+		if (tmp->next != NULL)
+			tmp->next->prev = tmp->prev;
 	}
 
-	free(temp);
+	free(tmp);
 	return (1);
-}
-
-/**
-  * get_dnodeint_at_index - Gets a node from a doubly linked list
-  * @head: The head of the doubly linked list
-  * @index: The index to find in the doubly linked list
-  *
-  * Return: The specific node of the doubly linked list
-  */
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
-{
-	dlistint_t *current = head;
-	unsigned int iter_times = 0;
-
-	if (head)
-	{
-		while (current != NULL)
-		{
-			if (iter_times == index)
-				return (current);
-
-			current = current->next;
-			++iter_times;
-		}
-	}
-
-	return (NULL);
-}
-
-/**
-  * dlistint_len - Counts the number of elements in a doubly linked list
-  * @h: The double linked list to count
-  *
-  * Return: Number of elements in the doubly linked list
-  */
-size_t dlistint_len(const dlistint_t *h)
-{
-	int lenght = 0;
-
-	while (h != NULL)
-	{
-		++lenght;
-		h = h->next;
-	}
-
-	return (lenght);
 }
